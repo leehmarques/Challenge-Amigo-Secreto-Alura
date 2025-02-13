e87542e2078883df3903f0b91fcb62d68e0f1832
Leticia Marques <lemarquesstudies@gmail.com>
2025-02-13 14:37:46 -0300

    Primeiro commit

---
+++ app.js
@@ -0,0 +1,53 @@
+let amigos = [];
+
+function adicionarAmigo() {
+    let input = document.getElementById("amigo");
+    let nome = input.value.trim();
+
+    if (nome != "" && nome.match(/^[a-zA-Z\s]+$/)) {
+        // adiciona ao array
+        amigos.push(nome);
+
+        // cria o li 
+        let lista = document.getElementById("listaAmigos");
+        let item = document.createElement("li");
+
+        // adiciona a lista na tela
+        item.textContent = nome;
+        lista.appendChild(item);
+
+        // limpar o input depois de adicionar
+        input.value = "";
+    } else {
+        alert("Por favor, digite um nome válido!");
+    }
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("Não há nomes para adicionar");
+        return;
+    }
+
+    let i = Math.floor(Math.random() * amigos.length);
+
+    let nomeSorteado = amigos[i];
+
+    let resultado = document.getElementById("resultado");
+    resultado.innerHTML = "";
+
+    let itemResultado = document.createElement("li");
+    itemResultado.textContent = `O amigo sorteado é ${nomeSorteado}`;
+    resultado.appendChild(itemResultado);
+}
+
+function reiniciarJogo() {
+    amigos = [];
+
+    let listaAmigos = document.getElementById("listaAmigos");
+    listaAmigos.innerHTML = "";
+
+    let listaResultado = document.getElementById("resultado");
+    listaResultado.innerHTML = "";
+}
+
